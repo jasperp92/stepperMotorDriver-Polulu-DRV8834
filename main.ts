da8c561cab1dda1ecf2d37cb3eff510bbdda94b0
Jasper <jasper.precht@web.de>
2022-06-27 13:35:35 +0200

    added more microsteps

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
-/*
-    /* Made from pxt-ucl-junkrobot and Learning Developments tutorial on stepper motors
-    /* https://github.com/chevyng/pxt-ucl-junkrobot/blob/master/main.ts
-    /* https://learningdevelopments.co.nz/blogs/lessons/stepper-motor-control-with-the-micro-bit
-    /*
-    /* And much help from Sparkfun's tutorial on building extensions in MakeCode
-    /* https://learn.sparkfun.com/tutorials/how-to-create-a-makecode-package-for-microbit/all
-    /* Much love from Tinkercademy
+/* very basic stepper library for bipolar stepper motor driver DRV8834. Others not tested yet.
+code based and inspired by Tinkertanker https://github.com/tinkertanker/pxt-stepper-motor
+
 */
 
 enum stepUnit {
@@ -28,7 +23,13 @@ enum stepMode {
     //% block="halfstep"
     halfStep,
     //% block="1/4 step"
-    oneFourthStep
+    quarterStep,
+    //% block="1/8 step"
+    eightStep,
+    //% block="1/16 step"
+    sixteenthStep,
+    //% block="1/32 step"
+    thirtysecondStep 
 }
 
 //% color=#a754d3 icon="\uf013"
@@ -54,7 +55,7 @@ namespace Polulu_DRV8834 {
             this.input5 = sleep;
         }
 
-        //% blockId=set_motor_calibration block="%motor|set delay between steps to %delayNum|ms"
+        //% blockId=set_motor_calibration block="%motor|set step duration %delayNum|ms"
         //% weight=60 blockGap=8
         setDelay(delayNum: number): void {
             this.delay = delayNum;
@@ -85,18 +86,39 @@ namespace Polulu_DRV8834 {
             }
         }
 
-        //% blockId=set_stepsPerRevolution block="%motor|set steps per revolution to %spr|"
+
+        //% blockId=set_stepsPerRevolution block="%motor|set steps per revolution (fullstep) to %spr|"
         //% weight=50 blockGap=8
         setStepsPerRevolution(sprNum: number): void {
             if(this.mode == stepMode.halfStep) {
             this.spr = sprNum * 2;
-            } else if(this.mode == stepMode.oneFourthStep) {
-            this.spr = sprNum * 4;
+            } else if(this.mode == stepMode.quarterStep) {
+                this.spr = sprNum * 4;
+            } else if(this.mode == stepMode.eightStep) {
+                this.spr = sprNum * 8;
+            } else if(this.mode == stepMode.sixteenthStep) {
+                this.spr = sprNum * 16;
+            } else if(this.mode == stepMode.thirtysecondStep) {
+                this.spr = sprNum * 32;
             } else {
             this.spr = sprNum;
             }
         }
+        
 
+                /*
+        * Step mode truth table
+        * M1 M0    step mode
+        *  0  0     1
+        *  0  1     2
+        *  0  Z     4
+        *  1  0     8
+        *  1  1    16
+        *  1  Z    32
+        *
+        *  Z = high impedance mode (M0 is tri-state)
+        */
+        
         //% blockId=set_stepMode block="%motor|set step mode to %mode|"
         //% weight=50 blockGap=8
         setStepMode(mode: stepMode): void {
@@ -107,12 +129,30 @@ namespace Polulu_DRV8834 {
                 case stepMode.halfStep:
                     pins.digitalWritePin(this.input3, 1);
                     pins.digitalWritePin(this.input4, 0);
-                case stepMode.oneFourthStep:
-                    pins.setPull(this.input3, PinPullMode.PullNone)
-                    pins.digitalWritePin(this.input4, 0);                    
+                case stepMode.quarterStep:
+                   // Z pins.setPull(this.input3, PinPullMode.PullNone)
+                    pins.digitalWritePin(this.input4, 0);
+                case stepMode.eightStep:
+                    pins.digitalWritePin(this.input3, 0);
+                    pins.digitalWritePin(this.input4, 1);
+                case stepMode.sixteenthStep:
+                    pins.digitalWritePin(this.input3, 1);
+                    pins.digitalWritePin(this.input4, 1);
+                case stepMode.thirtysecondStep:
+                    pins.digitalWritePin(this.input4, 1);
             }
             this.mode = mode;
          }
+
+        //% blockId=motorSleep block="%motor| sleep %sleep|"
+        //% weight=30 blockGap=8
+        motorSleep(sleep: boolean): void {
+            if(sleep) {
+                pins.digitalWritePin(this.input5, 0); 
+            } else {
+                pins.digitalWritePin(this.input5, 1);   
+            }    
+        }
     }
 
 
@@ -120,12 +160,12 @@ namespace Polulu_DRV8834 {
      * Create a new stepper motor with connected pins at @param.
      * @param 5 pins where the motor is connected. Sleep param is optional
      */
-    //% blockId="stepperMotor_setMotor" block="configure DRV8834 | steps %steps|dir %dir|M1 %M1|M0 %M0||sleep %sleep"
+    //% blockId="stepperMotor_setMotor" block="configure pins | steps %steps|dir %dir|M1 %M1|M0 %M0||sleep %sleep"
     //% weight=90 blockGap=8
     //% parts="motor"
     //% blockSetVariable=motor
     //% expandableArgumentMode="toggle"
-    //% steps.defl=DigitalPin.P0 dir.defl=DigitalPin.P1 sleep.defl=0
+    //% steps.defl=DigitalPin.P0 dir.defl=DigitalPin.P1 sleep.defl=1
     export function createMotor(steps: DigitalPin, dir: DigitalPin, M0: DigitalPin, M1: DigitalPin, sleep?: DigitalPin): Motor {
         let motor = new Motor();
         motor.setPins(steps, dir, M0, M1, sleep);
